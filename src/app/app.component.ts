8edf7b62bf0f60ccdb1f143ecfb743ca6d90bb0f
salarsen <larsen.spencer@gmail.com>
2018-04-05 20:44:32 -0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  acceptedMimeTypes = [
+    'image/gif',
+    'image/jpeg',
+    'image/png'
+  ];
+
+  @ViewChild('fileInput') fileInput: ElementRef;
+  fileDataUri = '';
+  errorMsg = '';
+
+  constructor(private http: HttpClient) { }
+
+  previewFile() {
+    const file = this.fileInput.nativeElement.files[0];
+    if (file && this.validateFile(file)) {
+
+      const reader = new FileReader();
+      reader.readAsDataURL(this.fileInput.nativeElement.files[0]);
+      reader.onload = () => {
+        this.fileDataUri = reader.result;
+      }
+    } else {
+      this.errorMsg = 'File must be jpg, png, or gif and cannot be exceed 500 KB in size'
+    }
+  }
+
+  uploadFile(event: Event) {
+    event.preventDefault();
+
+    // get only the base64 file and post it
+    if (this.fileDataUri.length > 0) {
+      const base64File = this.fileDataUri.split(',')[1];
+      const data = { 'image': base64File };
+      this.http.post(`${environment.apiUrl}/upload-photo`, data)
+        .subscribe(
+          res => {
+            // handle success
+            // reset file input
+            this.fileInput.nativeElement.value = '';
+          },
+          err => {
+            this.errorMsg = 'Could not upload image.';
+          }
+        );
+    }
+
+  }
+
+  validateFile(file) {
+    return this.acceptedMimeTypes.includes(file.type) && file.size < 500000;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -10,9 +11,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
